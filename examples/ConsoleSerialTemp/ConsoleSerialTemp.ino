7a922757536e7550e01fddef58f1ab2525da16fe
Isaac <isaacr100@verizon.net>
2017-10-02 20:27:49 -0400

    Update ConsoleSerialTemp.ino
    don't need license and addresses #2

--- examples/ConsoleSerialTemp/ConsoleSerialTemp.ino
+++ examples/ConsoleSerialTemp/ConsoleSerialTemp.ino
@@ -2,20 +2,8 @@
  
  Copyright 2017 Isaac100
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-
  This exmaple writes the current temperarure 
- in celcius and fahrenheit to the console.
+ in celsius and fahrenheit to the console.
  */
 
 #include <TMP36.h>
@@ -35,15 +23,15 @@ void setup() {
 void loop() {
   //create a variable and store the current temperature in
   //celius in it using the getTempC function
-  float celcius = myTMP36.getTempC(); 
+  float celsius = myTMP36.getTempC(); 
   
   //create a variable and store the current temperature in
   //fahrenheit in it using the getTempF function
   float fahrenheit = myTMP36.getTempF(); 
 
   //Print the data to the Console monitor
-  Console.print("Celcius: ");
-  Console.print(celcius);
+  Console.print("Celsius: ");
+  Console.print(celsius);
   Console.print(" Fahrenheit: ");
   Console.println(fahrenheit);
   delay(500);
